e1a9e8e22b760b02bd49b5aef4da3920a5c0dc27
benjamin <benjamin.slabbert@gmail.com>
2025-11-01 19:15:31 +0100

    update JUL logging

--- src/main/java/github/benslabbert/vdnative/Main.java
+++ src/main/java/github/benslabbert/vdnative/Main.java
@@ -1,18 +1,32 @@
 /* Licensed under Apache-2.0 2025. */
 package github.benslabbert.vdnative;
 
+import io.vertx.core.VertxException;
 import io.vertx.launcher.application.HookContext;
 import io.vertx.launcher.application.VertxApplication;
 import io.vertx.launcher.application.VertxApplicationHooks;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class Main extends VertxApplication {
 
+  static {
+    try (var is =
+        Thread.currentThread().getContextClassLoader().getResourceAsStream("logging.properties")) {
+      LogManager.getLogManager().readConfiguration(is);
+    } catch (IOException e) {
+      throw VertxException.noStackTrace(e);
+    }
+  }
+
   private static final Logger log = Logger.getLogger(Main.class.getName());
 
   public static void main(String[] args) {
+
     int code = new Main(args).launch();
-    log.info("Launching code %d".formatted(code));
+    log.log(Level.INFO, "Launching code {0}", code);
   }
 
   private Main(String[] args) {
@@ -31,6 +45,11 @@ public class Main extends VertxApplication {
                 .setWorkerPoolSize(1)
                 .setEventLoopPoolSize(1)
                 .setInternalBlockingPoolSize(1);
+          }
+
+          @Override
+          public void afterVertxStopped(HookContext context) {
+            log.info("Vertx stopped");
           }
         });
   }
