1b163a2f0e81916b8a0a39a9359315192fe8c3c0
benjamin <benjamin.slabbert@gmail.com>
2025-11-02 16:42:09 +0100

    update plugin, update build

--- transformer/src/main/java/github/benslabbert/vdnative/TransformerPlugin.java
+++ transformer/src/main/java/github/benslabbert/vdnative/TransformerPlugin.java
@@ -1,29 +1,18 @@
 /* Licensed under Apache-2.0 2025. */
 package github.benslabbert.vdnative;
 
-import java.util.List;
 import net.bytebuddy.asm.Advice;
 import net.bytebuddy.build.Plugin;
 import net.bytebuddy.description.method.MethodDescription.InDefinedShape;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.dynamic.ClassFileLocator;
 import net.bytebuddy.dynamic.DynamicType.Builder;
-import net.bytebuddy.matcher.ElementMatcher;
 
 public class TransformerPlugin implements Plugin {
 
   @Override
   public Builder<?> apply(Builder<?> builder, TypeDescription target, ClassFileLocator clf) {
-    List<InDefinedShape> advisedMethods =
-        target.getDeclaredMethods().stream()
-            .filter(s -> s.getDeclaredAnnotations().isAnnotationPresent(EnhanceMe.class))
-            .toList();
-
-    if (advisedMethods.isEmpty()) {
-      return builder;
-    }
-
-    for (InDefinedShape tm : advisedMethods) {
+    for (InDefinedShape tm : target.getDeclaredMethods()) {
       builder = builder.method(md -> md.equals(tm)).intercept(Advice.to(EnhanceAdvice.class));
     }
 
@@ -37,12 +26,7 @@ public class TransformerPlugin implements Plugin {
 
   @Override
   public boolean matches(TypeDescription typeDefinitions) {
-    return matcher().matches(typeDefinitions);
-  }
-
-  ElementMatcher<TypeDescription> matcher() {
-    return target ->
-        target.getDeclaredMethods().stream()
-            .anyMatch(s -> s.getDeclaredAnnotations().isAnnotationPresent(EnhanceMe.class));
+    return typeDefinitions.getDeclaredMethods().stream()
+        .anyMatch(s -> s.getDeclaredAnnotations().isAnnotationPresent(EnhanceMe.class));
   }
 }
